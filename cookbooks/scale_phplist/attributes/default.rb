91eb30a1bf658e7c610107efb2338b4daf06100b
Ilan Rabinovitch <ilan@datadoghq.com>
2016-07-02 15:57:55 -0700

    set db host for phplists in production

--- cookbooks/scale_phplist/attributes/default.rb
+++ cookbooks/scale_phplist/attributes/default.rb
@@ -7,10 +7,15 @@ if File.exists?('/etc/lists_secrets')
 end
 
 default['scale_phplist'] = {
-  'mysql_host' => d['mysql_host'] || 'db1',
   'mysql_db' => d['mysql_db'] || 'phplists',
   'mysql_user' => d['mysql_user'] || 'phplists',
   'mysql_password' => d['mysql_password'] || 'devpassword',
 }
+
+if node.vagrant?
+  default['scale_apache']['mysql_host'] = 'db1'
+else
+  default['scale_apache']['mysql_host'] = '26b289196faa9f09e8b99de13aa19528986e9b68.rackspaceclouddb.com'
+end
 
 default[scale_phplist]['version'] = '3.2.5'
