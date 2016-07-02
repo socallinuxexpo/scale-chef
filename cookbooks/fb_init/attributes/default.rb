485fa721487bbd90f0cc4fc21574640b9a83ec64
Ilan Rabinovitch <ilan@datadoghq.com>
2016-07-02 14:33:43 -0700

    fix parsing of run lists

--- cookbooks/fb_init/attributes/default.rb
+++ cookbooks/fb_init/attributes/default.rb
@@ -1,7 +1,10 @@
 # vim: syntax=ruby:expandtab:shiftwidth=2:softtabstop=2:tabstop=2
 
 # This stuff should go in an ohai plugin or some-such
-tier = node['fqdn'].split('.')[0].tr('0-9', '')
+tier = node['fqdn'].split('.')[0].tr('0-9', '').split('-')[1]
+org = node['fqdn'].split('.')[0].tr('0-9', '').split('-')[1]
+
 default['tier'] = tier
+default['org'] = org
 
 default['fb_init'] = {}
