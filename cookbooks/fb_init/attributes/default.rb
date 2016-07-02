6a1d94b925a929fa60b62eb337ef283eb2ef4ac4
Ilan Rabinovitch <ilan@datadoghq.com>
2016-07-02 14:36:12 -0700

    adjust hostname in vagrant file

--- cookbooks/fb_init/attributes/default.rb
+++ cookbooks/fb_init/attributes/default.rb
@@ -2,7 +2,7 @@
 
 # This stuff should go in an ohai plugin or some-such
 tier = node['fqdn'].split('.')[0].tr('0-9', '').split('-')[1]
-org = node['fqdn'].split('.')[0].tr('0-9', '').split('-')[1]
+org = node['fqdn'].split('.')[0].tr('0-9', '').split('-')[0]
 
 default['tier'] = tier
 default['org'] = org
