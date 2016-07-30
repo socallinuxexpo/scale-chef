51bda6932f4f1e348e15b128f3457cb21121947c
Ilan Rabinovitch <ilan@datadoghq.com>
2016-07-30 12:10:08 -0700

    pull mysqlhost from list_secrets

--- cookbooks/scale_phplist/attributes/default.rb
+++ cookbooks/scale_phplist/attributes/default.rb
@@ -10,13 +10,7 @@ default['scale_phplist'] = {
   'mysql_db' => d['mysql_db'] || 'phplists',
   'mysql_user' => d['mysql_user'] || 'phplists',
   'mysql_password' => d['mysql_password'] || 'devpassword',
+  'mysql_host' => d['mysql_host'] || 'db1',
 }
-
-if node.vagrant?
-  default['scale_apache']['mysql_host'] = 'db1'
-else
-  default['scale_apache']['mysql_host'] =
-    '26b289196faa9f09e8b99de13aa19528986e9b68.rackspaceclouddb.com'
-end
 
 default[scale_phplist]['version'] = '3.2.5'
