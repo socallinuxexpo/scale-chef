c704a668f8a11c5f1fd7973668ecfa80a61bb473
Ilan Rabinovitch <ilan@datadoghq.com>
2016-07-31 15:11:02 -0700

    remove spurious quote

--- cookbooks/scale_phplist/attributes/default.rb
+++ cookbooks/scale_phplist/attributes/default.rb
@@ -13,10 +13,9 @@ default['scale_phplist'] = {
   'mysql_host' => d['mysql_host'] || 'db1',
   'bounce_mailbox_host' => d['bounce_mailbox_host'],
   'bounce_mailbox_user' => d['bounce_mailbox_user'],
-  'bounce_mailbox_password' => d['bounce_mailbox_password'] || '',
+  'bounce_mailbox_password' => d['bounce_mailbox_password'],
 }
 
 default[scale_phplist]['version'] = '3.2.5'
 default[scale_phplist]['bounce_protocol'] = 'pop'
 default[scale_phplist]['bounce_mailbox_port'] = '587/pop3/tls'
-'
