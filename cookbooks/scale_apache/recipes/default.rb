f7eaf2a239af610e5cda9bcc5bfedf07ebf61bab
Phil Dibowitz <phil@ipom.com>
2016-10-03 13:50:35 +0200

    Minor fixes to bootstrapping and testing

--- cookbooks/scale_apache/recipes/default.rb
+++ cookbooks/scale_apache/recipes/default.rb
@@ -7,6 +7,12 @@
 # All rights reserved - Do Not Redistribute
 #
 
+directory '/etc/httpd' do
+  owner 'root'
+  group 'root'
+  mode '0755'
+end
+
 cookbook_file '/etc/httpd/sf_bundle.crt' do
   owner 'root'
   group 'root'
