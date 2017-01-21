aeb190ccb5bc1220da822421d76d7b1965318cea
Davide Cavalca <dcavalca@fb.com>
2017-01-20 21:14:34 -0800

    scale_yum: use current epel package

--- cookbooks/scale_yum/recipes/default.rb
+++ cookbooks/scale_yum/recipes/default.rb
@@ -1,8 +1,10 @@
 # vim:shiftwidth=2:expandtab
 
-remote_file "#{Chef::Config['file_cache_path']}/epel-release-7-8.noarch.rpm" do
+epel_pkg = 'epel-release-7-9.noarch.rpm'
+
+remote_file "#{Chef::Config['file_cache_path']}/#{epel_pkg}" do
   not_if { File.exists?('/etc/yum.repos.d/epel.repo') }
-  source 'http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-8.noarch.rpm'
+  source "http://dl.fedoraproject.org/pub/epel/7/x86_64/e/#{epel_pkg}"
   owner 'root'
   group 'root'
   mode '0644'
@@ -11,6 +13,6 @@ end
 
 package 'epel-release' do
   not_if { File.exists?('/etc/yum.repos.d/epel.repo') }
-  source "#{Chef::Config['file_cache_path']}/epel-release-7-8.noarch.rpm"
+  source "#{Chef::Config['file_cache_path']}/#{epel_pkg}"
 end
 
