39f610302ef81495d41c5b4fbd857e0934fbbaa2
Phil Dibowitz <phil@ipom.com>
2017-07-18 22:32:18 -0400

    More SSl fixes for new certs

--- cookbooks/scale_apache/recipes/dev.rb
+++ cookbooks/scale_apache/recipes/dev.rb
@@ -1,7 +1,6 @@
 execute 'generate dev keys' do
-  creates '/etc/httpd/apache.pem'
-  command 'openssl req -x509 -newkey rsa:2048 -keyout /etc/httpd/key.pem ' +
-      '-out /etc/httpd/cert.pem -days 999 -nodes -subj ' +
-      '"/O=scale/countryName=US/commonName=www.socallinuxexpo.org"' +
-      '; cat /etc/httpd/key.pem /etc/httpd/cert.pem > /etc/httpd/apache.pem'
+  creates '/etc/httpd/apache.key'
+  command 'openssl req -x509 -newkey rsa:2048 -keyout /etc/httpd/apache.pem ' +
+      '-out /etc/httpd/apache.pem -days 999 -nodes -subj ' +
+      '"/O=scale/countryName=US/commonName=www.socallinuxexpo.org"'
 end
