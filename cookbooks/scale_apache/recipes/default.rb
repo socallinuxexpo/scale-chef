858d5f9690d92fd935898eefae893c46256ea8c6
Ilan Rabinovitch <ilan@datadoghq.com>
2017-09-07 00:21:21 -0400

    update scale redirects to 16x

--- cookbooks/scale_apache/recipes/default.rb
+++ cookbooks/scale_apache/recipes/default.rb
@@ -109,9 +109,15 @@ rewrites = {
     ],
   },
   'redirect / to current site' => {
-    'rule' => '^/(.*) http://www.socallinuxexpo.org/scale/15x [L,R,NE]',
+    'rule' => '^/(.*) http://www.socallinuxexpo.org/scale/16x [L,R,NE]',
     'conditions' => [
       '%{REQUEST_URI} ^/$',
+    ],
+  },
+  'redirect short url to proper url' => {
+    'rule' => '^/(.*) http://www.socallinuxexpo.org/scale/16x [L,R,NE]',
+    'conditions' => [
+      '%{REQUEST_URI} ^/scale16x$',
     ],
   },
   'redirect short url to proper url' => {
