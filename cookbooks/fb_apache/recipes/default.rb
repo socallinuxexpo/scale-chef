dc12a62c7c42d1a2e5de7044d72605af6d632aab
Phil Dibowitz <phil@ipom.com>
2019-07-21 21:33:55 -0700

    Update fb_apache to latest upstream
    There's a few fixes and features.
    
    Tested on scale-web1 and it worked fine.
    
    Signed-off-by: Phil Dibowitz <phil@ipom.com>

--- cookbooks/fb_apache/recipes/default.rb
+++ cookbooks/fb_apache/recipes/default.rb
@@ -5,25 +5,64 @@
 # Copyright (c) 2016-present, Facebook, Inc.
 # All rights reserved.
 #
-# This source code is licensed under the BSD-style license found in the
-# LICENSE file in the root directory of this source tree. An additional grant
-# of patent rights can be found in the PATENTS file in the same directory.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 #
 
-confdir = value_for_platform_family(
-  'rhel' => '/etc/httpd/conf.d',
-  'debian' => '/etc/apache2/conf.d',
-)
+apache_version =
+  case node['platform_family']
+  when 'debian'
+    case node['platform']
+    when 'ubuntu'
+      node['platform_version'].to_f >= 13.10 ? '2.4' : '2.2'
+    when 'debian'
+      node['platform_version'].to_f >= 8.0 ? '2.4' : '2.2'
+    else
+      '2.4'
+    end
+  when 'rhel'
+    node['platform_version'].to_f >= 7.0 ? '2.4' : '2.2'
+  end
+
+confdir =
+  case node['platform_family']
+  when 'rhel'
+    '/etc/httpd/conf.d'
+  when 'debian'
+    case apache_version
+    when '2.2'
+      '/etc/apache2/conf.d'
+    when '2.4'
+      '/etc/apache2/conf-enabled'
+    end
+  end
 
 sitesdir = value_for_platform_family(
   'rhel' => confdir,
   'debian' => '/etc/apache2/sites-enabled',
 )
 
-moddir = value_for_platform_family(
-  'rhel' => '/etc/httpd/conf.modules.d',
-  'debian' => '/etc/apache2/modules-enabled',
-)
+moddir =
+  case node['platform_family']
+  when 'rhel'
+    '/etc/httpd/conf.modules.d'
+  when 'debian'
+    case apache_version
+    when '2.2'
+      '/etc/apache2/modules-enabled'
+    when '2.4'
+      '/etc/apache2/mods-enabled'
+    end
+  end
 
 sysconfig = value_for_platform_family(
   'rhel' => '/etc/sysconfig/httpd',
@@ -59,6 +98,14 @@ template sysconfig do
   notifies :restart, 'service[apache]'
 end
 
+[moddir, sitesdir, confdir].uniq.each do |dir|
+  directory dir do
+    owner 'root'
+    group 'root'
+    mode '0755'
+  end
+end
+
 template "#{moddir}/fb_modules.conf" do
   not_if { node.centos6? }
   owner 'root'
@@ -71,14 +118,14 @@ template "#{sitesdir}/fb_sites.conf" do
   owner 'root'
   group 'root'
   mode '0644'
-  notifies :restart, 'service[apache]'
+  notifies :reload, 'service[apache]'
 end
 
 template "#{confdir}/fb_apache.conf" do
   owner 'root'
   group 'root'
   mode '0644'
-  notifies :restart, 'service[apache]'
+  notifies :reload, 'service[apache]'
 end
 
 service 'apache' do
