84b80896baa22c1d491a823fe469796cc9ae82ff
Phil Dibowitz <phil@ipom.com>
2020-04-14 20:15:02 -0700

    Fix runlists

--- cookbooks/fb_init/attributes/default.rb
+++ cookbooks/fb_init/attributes/default.rb
@@ -1,19 +1,14 @@
 # vim: syntax=ruby:expandtab:shiftwidth=2:softtabstop=2:tabstop=2
 
 # This stuff should go in an ohai plugin or some-such
-shorthostname = node['fqdn'].split('.')[0]
+shorthostname = node['hostname']
 trimmed_hostname = shorthostname.tr('0-9', '')
 pieces = trimmed_hostname.split('-')
-if pieces.size == 3
-  env = pieces[2]
-else
-  env = 'prod'
-end
-org = pieces[1]
-tier = pieces[0]
+
+env = 'prod'
+tier = pieces[1]
 
 default['tier'] = tier
-default['org'] = org
 default['env'] = env
 
 default['fb_init'] = {}
