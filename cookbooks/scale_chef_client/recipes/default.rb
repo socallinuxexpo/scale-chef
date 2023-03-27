556102c3a4ac32f717d62f4d29dc9b861b552f30
Phil Dibowitz <phil@ipom.com>
2023-03-26 22:10:06 -0700

    Use appropriate release of CINC
    Signed-off-by: Phil Dibowitz <phil@ipom.com>

--- cookbooks/scale_chef_client/recipes/default.rb
+++ cookbooks/scale_chef_client/recipes/default.rb
@@ -9,10 +9,11 @@
 
 # omfg I hate this so much
 version = '17.10.0'
-rpm = "cinc-#{version}-1.el7.x86_64.rpm"
+rpm = "cinc-#{version}-1.el#{node['platform_version']}.x86_64.rpm"
 rpmpath = File.join(Chef::Config['file_cache_path'], rpm)
 remote_file rpmpath do
-  source "http://downloads.cinc.sh/files/stable/cinc/#{version}/el/7/#{rpm}"
+  source "http://downloads.cinc.sh/files/stable/cinc/#{version}/el/" +
+    "#{node['platform_version']}/#{rpm}"
   owner 'root'
   group 'root'
   mode '0644'
