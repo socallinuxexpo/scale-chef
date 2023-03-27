1385087876b281ea6587ec5e253d834103a79010
Phil Dibowitz <phil@ipom.com>
2023-03-26 22:00:35 -0700

    EPEL support for CentOS8S and beyond
    Signed-off-by: Phil Dibowitz <phil@ipom.com>

--- cookbooks/scale_yum/recipes/default.rb
+++ cookbooks/scale_yum/recipes/default.rb
@@ -1,18 +1,23 @@
 # vim:shiftwidth=2:expandtab
 
-epel_pkg = 'epel-release-7-12.noarch.rpm'
+if node.centos7?
+  epel_pkg = 'epel-release-7-12.noarch.rpm'
 
-remote_file "#{Chef::Config['file_cache_path']}/#{epel_pkg}" do
-  not_if { File.exists?('/etc/yum.repos.d/epel.repo') }
-  source "https://dl.fedoraproject.org/pub/epel/7/x86_64/Packages/e/#{epel_pkg}"
-  owner 'root'
-  group 'root'
-  mode '0644'
-  action :create
+  remote_file "#{Chef::Config['file_cache_path']}/#{epel_pkg}" do
+    not_if { File.exists?('/etc/yum.repos.d/epel.repo') }
+    source "https://dl.fedoraproject.org/pub/epel/7/x86_64/Packages/e/#{epel_pkg}"
+    owner 'root'
+    group 'root'
+    mode '0644'
+    action :create
+  end
+
+  package 'epel-release' do
+    not_if { File.exists?('/etc/yum.repos.d/epel.repo') }
+    source "#{Chef::Config['file_cache_path']}/#{epel_pkg}"
+  end
+else
+  package 'epel-release' do
+    action :upgrade
+  end
 end
-
-package 'epel-release' do
-  not_if { File.exists?('/etc/yum.repos.d/epel.repo') }
-  source "#{Chef::Config['file_cache_path']}/#{epel_pkg}"
-end
-
