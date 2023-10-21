b8a894d6a8aef7f7310962cdb99bff19a6eea92a
Phil Dibowitz <phil@ipom.com>
2023-10-21 13:58:03 -0700

    Restart apache only every 2 hours
    Summary:
    
    Logs look good
    
    Test Plan:
    
    none

--- cookbooks/scale_apache/recipes/default.rb
+++ cookbooks/scale_apache/recipes/default.rb
@@ -13,8 +13,9 @@ include_recipe 'fb_apache'
 apache_debug_log = '/var/log/apache_status.log'
 if node['hostname'] == 'scale-web2'
   cron 'ugly restarts' do
-    # once an hour
+    # once every other hour
     minute '02'
+    hour '*/2'
     command "date >> #{apache_debug_log}; " +
       "ps auxwww | grep http >> #{apache_debug_log}; " +
       '/usr/bin/systemctl restart httpd'
