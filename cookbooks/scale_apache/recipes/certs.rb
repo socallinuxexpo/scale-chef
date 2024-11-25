24aa4662256ba615d165ce96e121e2c1ed790b69
Phil Dibowitz <phil@ipom.com>
2024-11-25 09:41:21 -0800

    Add venv workaround for CentOS10 cerbot
    taste-tested on web and ran renew_certs

--- cookbooks/scale_apache/recipes/certs.rb
+++ cookbooks/scale_apache/recipes/certs.rb
@@ -11,6 +11,20 @@ unless pkgs.length.zero?
   end
 end
 
+if node.centos10?
+  cachefile = "#{Chef::Config['file_cache_path']}/certbot-venv.tar.bz2"
+  cookbook_file cachefile do
+    owner 'root'
+    group 'root'
+    mode '0640'
+  end
+
+  archive_file cachefile do
+    destination '/usr/local/certbot-venv'
+    strip_components 1
+  end
+end
+
 # Always run our renewal script
 cookbook_file '/usr/local/sbin/renew_certs.sh' do
   owner 'root'
