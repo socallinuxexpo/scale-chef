c7e3e7faa6dcbef9e7f58c583b1cb8690ffadcf3
Phil Dibowitz <phil@ipom.com>
2025-04-07 13:57:00 -0700

    Reg: now with containers and centos 10! (#365)
    * leaves existing reg alone
    * reg now works on the new box!
    
    Signed-off-by: Phil Dibowitz <phil@ipom.com>

--- cookbooks/scale_reg/metadata.rb
+++ cookbooks/scale_reg/metadata.rb
@@ -7,3 +7,5 @@ source_url 'https://github.com/socallinuxexpo/scale-chef'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 depends 'scale_apache'
+depends 'fb_nginx'
+depends 'scale_certbot_hack'
