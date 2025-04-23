b6a07e75984130ff69c9d193e4474a34b6a25ecb
Phil Dibowitz <phil@ipom.com>
2025-04-23 14:25:48 -0700

    Add timesync with chrony
    We had no timesync. :( This adds chrony and has been
    tested on lists and reg
    
    Signed-off-by: Phil Dibowitz <phil@ipom.com>

--- cookbooks/fb_init/metadata.rb
+++ cookbooks/fb_init/metadata.rb
@@ -8,6 +8,7 @@ source_url 'https://github.com/socallinuxexpo/scale-chef'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.0.1'
 %w{
+  fb_chrony
   fb_cron
   fb_dnf
   fb_fstab
