17a05db087577aa9702421943c1b3f9565a25106
Davide Cavalca <davide@socallinuxexpo.org>
2025-04-23 13:37:33 -0700

    fb_syslog: sync with upstream

--- cookbooks/fb_syslog/recipes/packages.rb
+++ cookbooks/fb_syslog/recipes/packages.rb
@@ -20,15 +20,20 @@
 return if node.macos?
 
 package 'rsyslog' do
+  only_if { node['fb_syslog']['manage_packages'] }
   action :upgrade
 end
 
-fb_systemd_override 'override' do
-  unit_name 'rsyslog.service'
-  content({
-            'Unit' => { 'Requires' => 'syslog.socket' },
-            'Install' => { 'Alias' => 'syslog.service' },
-          })
+# TODO(davide125): Document this
+if node.systemd?
+  fb_systemd_override 'override' do
+    only_if { node['fb_syslog']['_enable_syslog_socket_override'] }
+    unit_name 'rsyslog.service'
+    content({
+              'Unit' => { 'Requires' => 'syslog.socket' },
+              'Install' => { 'Alias' => 'syslog.service' },
+            })
+  end
 end
 
 include_recipe 'fb_syslog::enable'
