2682221221ae59a98abb3c462781ff33dcb2e070
Phil Dibowitz <phil@ipom.com>
2025-05-13 10:40:23 -0700

    Move to fb_letsencrypt [part 1] (#413)
    This moves all webserver certs to fb_letsencrypt.
    
    In a future PR I'll remove scale_certbot_hack and add some cleanup
    recipes too.
    
    Note this is built on top of #412, so both commits are in this PR, until
    that is merged, click commits and view only the top one.
    
    Signed-off-by: Phil Dibowitz <phil@ipom.com>

--- cookbooks/scale_reg/metadata.rb
+++ cookbooks/scale_reg/metadata.rb
@@ -8,4 +8,4 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 depends 'scale_apache'
 depends 'fb_nginx'
-depends 'scale_certbot_hack'
+depends 'fb_letsencrypt'
