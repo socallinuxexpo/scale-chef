f1a2b329e19d1e701523ad03fbab329f3875da88
Phil Dibowitz <phil@ipom.com>
2025-07-21 16:21:37 -0700

    Settings for newsite.
    Tested on existing machine, no changes.
    
    Signed-off-by: Phil Dibowitz <phil@ipom.com>

--- cookbooks/scale_drupal/attributes/default.rb
+++ cookbooks/scale_drupal/attributes/default.rb
@@ -1,5 +1,4 @@
 default['scale_drupal'] = {
   'drupal_database' => 'drupal',
-  'mysql_host' => node.vagrant? ? 'scale-db1' :
-    'scale-drupal.cluster-c19nohpiwnoo.us-east-1.rds.amazonaws.com',
+  'mysql_host' => 'scale-db1',
 }
