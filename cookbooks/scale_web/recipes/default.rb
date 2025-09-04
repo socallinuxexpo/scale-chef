e6f09203b29a99a631bed7e1f6fba0e48c6055e8
Ilan Rabinovitch <ilan@fonz.net>
2025-09-03 23:45:51 -0400

    adjust path for 13x

--- cookbooks/scale_web/recipes/default.rb
+++ cookbooks/scale_web/recipes/default.rb
@@ -136,7 +136,7 @@ base_config = common_config.merge({
     '/scale12x /home/webroot/scale12x',
     '/scale12x-supporting /home/webroot/scale12x-supporting',
     '/scale/23x /home/webroot/scale23x-static',
-    '/scale/13x /home/webroot/past/scale13x',
+    '/scale/13x /home/webroot/scale/scale13x',
 
     '/doc /usr/share/doc',
   ],
