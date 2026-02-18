2e9fbb877876fa90b3b661af68c4bfea9cbfc1a0
Phil Dibowitz <phil@ipom.com>
2026-02-17 16:15:52 -0800

    Reg->Listmonk email sync
    Pulls from reg, has dryrun, reports to DD
    
    Closes #364
    
    Signed-off-by: Phil Dibowitz <phil@ipom.com>

---
+++ cookbooks/scale_reg/attributes/default.rb
@@ -0,0 +1,7 @@
+default['scale_reg'] = {
+  'db' => {
+    'hostname' => 'scalereg.cluster-c19nohpiwnoo.us-east-1.rds.amazonaws.com',
+    'username' => 'admin',
+    'database' => 'scalereg',
+  },
+}
---
+++ cookbooks/scale_reg/recipes/email_sync.rb
@@ -0,0 +1,17 @@
+cookbook_file '/usr/local/bin/scale_email_sync' do
+  owner 'root'
+  group 'root'
+  mode '0755'
+end
+
+template '/etc/scale_email_sync.yml' do
+  owner 'apache'
+  group 'apache'
+  mode '0640'
+end
+
+node.default['fb_cron']['jobs']['scale_email_sync'] = {
+  'command' => '/usr/local/bin/scale_email_sync --prod-lists &>/dev/null',
+  'user' => 'apache',
+  'time' => '5 * * * *',
+}
