b71bd7d1998bd9de2b0ad3a755e6577705d38c45
Phil Dibowitz <phil@ipom.com>
2026-03-04 17:27:35 -0800

    Add missing cookbook (#611)
    Signed-off-by: Phil Dibowitz <phil@ipom.com>

---
+++ cookbooks/fb_apcupsd/metadata.rb
@@ -0,0 +1,12 @@
+# Copyright (c) 2016-present, Facebook, Inc.
+name 'fb_apcupsd'
+maintainer 'Facebook'
+maintainer_email 'noreply@facebook.com'
+license 'Apache-2.0'
+description 'Installs/Configures apcupsd'
+source_url 'https://github.com/facebook/chef-cookbooks/'
+version '0.1.0'
+supports 'centos'
+supports 'debian'
+supports 'ubuntu'
+depends 'fb_helpers'
